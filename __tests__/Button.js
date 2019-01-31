877cf5bb1f8ac5081853fb05a2ce71280ce5ca9b
Reactongraph <js.developer@ongraph.com>
2019-01-31 18:01:17 +0530

    Update snapshots and run test

--- __tests__/Button.js
+++ __tests__/Button.js
@@ -5,11 +5,14 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import Button from '../src/components/Button';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('button snapshots', () => {
+  const snap = renderer.create(
+    <Button />
+  ).toJSON();
+  expect(snap).toMatchSnapshot();
 });
